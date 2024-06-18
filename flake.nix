19e7822c29d440096aac2e525acc67e04d4a6f04
= <=>
2024-06-18 14:07:53 -0600

    fix flake

--- flake.nix
+++ flake.nix
@@ -32,11 +32,11 @@
           ./machines/thinkpad-p53/configuration.nix
         ];
 
-        home-manager.nixosModules.home-manager
+        home-manager.nixosModules.home-manager =
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-          }
+          };
       };
     };
   };
