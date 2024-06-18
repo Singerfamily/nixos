d0c213046938024983a2871cd5ed474a2dbd648f
LeaderbotX400 <eric@singerfamily.ca>
2024-06-17 19:16:03 -0600

    stuff

--- pkgs/zsh.nix
+++ pkgs/zsh.nix
@@ -8,9 +8,9 @@
 
     programs.zsh = {
         enable = true;
-        enableCompletion = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
+        # enableCompletion = true;
+        # autosuggestion.enable = true;
+        # syntaxHighlighting.enable = true;
 
         shellAliases = {
             ll = "ls -l";
