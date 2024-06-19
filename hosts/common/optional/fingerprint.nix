eff4d620f25fbb903ce0c3045eef740ea4c6f80d
LeaderbotX400 <eric@singerfamily.ca>
2024-06-19 17:41:40 -0600

    sddm override

--- hosts/common/optional/fingerprint.nix
+++ hosts/common/optional/fingerprint.nix
@@ -9,5 +9,12 @@
     fprintd
   ];
 
-  security.pam.services.fprintd.fprintAuth = true;
+    security.pam.services.sddm.text = pkgs.lib.mkForce ''
+    auth 			[success=1 new_authtok_reqd=1 default=ignore]  	pam_unix.so try_first_pass likeauth nullok
+    auth 			sufficient  	pam_fprintd.so
+    auth      substack      login
+    account   include       login
+    password  substack      login
+    session   include       login
+  '';
 }
--- hosts/common/users/esinger/default.nix
+++ hosts/common/users/esinger/default.nix
@@ -10,12 +10,14 @@
   users.users.esinger = {
     isNormalUser = true;
     shell = pkgs.zsh;
+    name = "esinger";
     extraGroups = [
       "wheel"
       "video"
       "audio"
       "networkmanager" 
       "docker"
+      "tss"
     ];
     hashedPassword = "$y$j9T$YphPlKG7g7gLEptn6BlZc0$dDzIl6DbbNc/2HYjzMnR6OaNnreOyEv5qyVSDZqRg10";
   };
