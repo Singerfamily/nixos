c2a1cb48856e4df963db89a521e561714ecaf6e8
LeaderbotX400 <eric@singerfamily.ca>
2024-06-19 09:29:32 -0600

    disable lanzaboote

--- flake.nix
+++ flake.nix
@@ -6,12 +6,12 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.1";
+    # lanzaboote = {
+    #   url = "github:nix-community/lanzaboote/v0.4.1";
 
-      # Optional but recommended to limit the size of your system closure.
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #   # Optional but recommended to limit the size of your system closure.
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs = { self, nixpkgs, home-manager, lanzaboote } @ inputs:
@@ -26,26 +26,26 @@
           modules = [ 
             ./hosts/thinkpad-p53/configuration.nix
 
-             lanzaboote.nixosModules.lanzaboote
+            #  lanzaboote.nixosModules.lanzaboote
 
-              ({ pkgs, lib, ... }: {
+            #   ({ pkgs, lib, ... }: {
 
-                environment.systemPackages = [
-                  # For debugging and troubleshooting Secure Boot.
-                  pkgs.sbctl
-                ];
+            #     environment.systemPackages = [
+            #       # For debugging and troubleshooting Secure Boot.
+            #       pkgs.sbctl
+            #     ];
 
-                # Lanzaboote currently replaces the systemd-boot module.
-                # This setting is usually set to true in configuration.nix
-                # generated at installation time. So we force it to false
-                # for now.
-                boot.loader.systemd-boot.enable = lib.mkForce false;
+            #     # Lanzaboote currently replaces the systemd-boot module.
+            #     # This setting is usually set to true in configuration.nix
+            #     # generated at installation time. So we force it to false
+            #     # for now.
+            #     boot.loader.systemd-boot.enable = lib.mkForce false;
 
-                boot.lanzaboote = {
-                  enable = true;
-                  pkiBundle = "/etc/secureboot";
-                };
-              })
+            #     boot.lanzaboote = {
+            #       enable = true;
+            #       pkiBundle = "/etc/secureboot";
+            #     };
+            #   })
           ];
         };
       };
