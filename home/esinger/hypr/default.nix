96f44b2d37673006b0135b63a9dbd6b1866d64be
LeaderbotX400 <eric@singerfamily.ca>
2024-06-25 12:19:07 -0600

    TEST

--- home/esinger/hypr/default.nix
+++ home/esinger/hypr/default.nix
@@ -2,56 +2,57 @@
 
   imports = [
     ./waybar.nix
+    ./rofi.nix
   ];
 
-  wayland.windowManager.hyprland = {
-    enable = true;
-    systemd.enable = true;
+  # wayland.windowManager.hyprland = {
+  #   enable = true;
+  #   systemd.enable = true;
     
-    plugins = [
-      # inputs.hyprland-plugins.packages."${pkgs.system}".hyprbars
-      # inputs.hyprland-plugins.packages."${pkgs.system}".borders-plus-plus
-    ];
+  #   plugins = [
+  #     # inputs.hyprland-plugins.packages."${pkgs.system}".hyprbars
+  #     # inputs.hyprland-plugins.packages."${pkgs.system}".borders-plus-plus
+  #   ];
 
 
-    settings = {
-      "$mod" = "SUPER";
+  #   settings = {
+  #     "$mod" = "SUPER";
 
-      animation = [
-        "workspaces,1,2,default"
-        "windows,1,2,default,slide"
-        "fade,0"
-      ];
+  #     animation = [
+  #       "workspaces,1,2,default"
+  #       "windows,1,2,default,slide"
+  #       "fade,0"
+  #     ];
 
-      bindm = [
-        "$mod, mouse:272, movewindow"
-        "$mod, mouse:273, resizewindow"
-        "$mod ALT, mouse:272, resizewindow"
-      ];
+  #     bindm = [
+  #       "$mod, mouse:272, movewindow"
+  #       "$mod, mouse:273, resizewindow"
+  #       "$mod ALT, mouse:272, resizewindow"
+  #     ];
 
-      bind =
-        [
-          "$mod, W, exec, microsoft-edge-dev"
-          ", Print, exec, grimblast copy area"
-          "$mod, Q, exec, kitty"
-          "$mod, L, exec"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-          builtins.concatLists (builtins.genList (
-              x: let
-                ws = let
-                  c = (x + 1) / 10;
-                in
-                  builtins.toString (x + 1 - (c * 10));
-              in [
-                "$mod, ${ws}, workspace, ${toString (x + 1)}"
-                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-              ]
-            )
-            10)
-        );
-      };
-  };
+  #     bind =
+  #       [
+  #         "$mod, W, exec, microsoft-edge-dev"
+  #         ", Print, exec, grimblast copy area"
+  #         "$mod, Q, exec, kitty"
+  #         "$mod, L, exec"
+  #       ]
+  #       ++ (
+  #         # workspaces
+  #         # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+  #         builtins.concatLists (builtins.genList (
+  #             x: let
+  #               ws = let
+  #                 c = (x + 1) / 10;
+  #               in
+  #                 builtins.toString (x + 1 - (c * 10));
+  #             in [
+  #               "$mod, ${ws}, workspace, ${toString (x + 1)}"
+  #               "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+  #             ]
+  #           )
+  #           10)
+  #       );
+  #     };
+  # };
 }
