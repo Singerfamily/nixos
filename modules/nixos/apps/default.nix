4c1bc73dbca70881a3f72707c3d449f0f1ecb0a4
LeaderbotX400 <eric@singerfamily.ca>
2024-06-27 13:07:44 -0600

    feat: Add Nix Flatpak support and update dependencies

--- modules/nixos/apps/default.nix
+++ modules/nixos/apps/default.nix
@@ -1,11 +1,9 @@
 {config, lib, ...}: {
   imports = [
     ./spotify.nix
-    ./flatpak.nix
     ./steam.nix
   ];
 
   config.spotify.enable = lib.mkDefault true;
-  config.flatpak.enable = lib.mkDefault true;
   config.steam.enable = lib.mkDefault false;
 }
