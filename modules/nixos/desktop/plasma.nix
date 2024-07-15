7862f2fe5f36d42de2bb3cce0bdcb23b05296a38
LeaderbotX400 <eric@singerfamily.ca>
2024-07-14 19:51:36 -0600

    feat: Enable Wayland support in SDDM display manager

--- modules/nixos/desktop/plasma.nix
+++ modules/nixos/desktop/plasma.nix
@@ -1,12 +1,14 @@
 {pkgs, ...}: {
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm ={
+    enable = true;
+    wayland.enable = true;
+  };
   services.desktopManager.plasma6.enable = true;
 
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+  # services.xserver.xkb = {
+  #   layout = "us";
+  #   variant = "";
+  # };
 
   environment.systemPackages = with pkgs; [
     aha
