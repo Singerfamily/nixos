dca16990f1632e44f15f99d3614c1e796c9c11d1
LeaderbotX400 <eric@singerfamily.ca>
2024-07-16 17:16:02 -0600

    chore: Update podman.nix with additional development tools

--- modules/nixos/global/podman.nix
+++ modules/nixos/global/podman.nix
@@ -17,9 +17,9 @@
   # Useful other development tools
   environment.systemPackages = with pkgs; [
     distrobox
-    dive # look into docker image layers
-    podman-tui # status of containers in the terminal
-    docker-compose # start group of containers for dev
+    dive            # look into docker image layers
+    podman-tui      # status of containers in the terminal
+    docker-compose  # start group of containers for dev
     #podman-compose # start group of containers for dev
   ];
 }
