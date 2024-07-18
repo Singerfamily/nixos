0701d34803339598ab9ebdea94749fb53c3292bb
LeaderbotX400 <eric@singerfamily.ca>
2024-07-18 17:27:33 -0600

    refactor: Update OpenRazer configuration for user 'esinger'
    This commit updates the OpenRazer configuration in the NixOS repository for the user 'esinger'. It adds 'esinger' to the list of users for OpenRazer, enabling the hardware features for that user. The previous system package 'prismlauncher' is commented out, as it is no longer needed. This change aims to improve the customization and management of OpenRazer settings.

--- hosts/event-horizon/configuration.nix
+++ hosts/event-horizon/configuration.nix
@@ -17,9 +17,11 @@
 
   hardware.openrazer.enable = true;
 
-  environment.systemPackages = [
-    prismlauncher
-  ];
+  hardware.openrazer.users = ["esinger"]; 
+
+  # environment.systemPackages = [
+  #   prismlauncher
+  # ];
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
