a4b08d4e0c5741f2a9df6b79b4307ea888cae7a2
LeaderbotX400 <eric@singerfamily.ca>
2024-07-18 15:41:59 -0600

    refactor: Update Flatpak module configuration
    This commit updates the Flatpak module configuration in the NixOS repository. It introduces a new `cfg` variable to hold the configuration for Flatpak. This change allows for easier customization and management of Flatpak-related settings.

--- modules/nixos/services/flatpak.nix
+++ modules/nixos/services/flatpak.nix
@@ -1,19 +1,20 @@
 { config, lib, ... }:
-
-{
+let
+  cfg = config.flatpak;
+in {
   options = {
     flatpak = {
       enable = lib.mkEnableOption "Enable Flatpak";
     };
   };
 
-  config = lib.mkIf config.flatpak.enable {
+  config = lib.mkIf cfg.enable {
     services.flatpak.enable = true;
     xdg.portal.enable = true;
 
-    services.flatpak.remotes = [{
-      name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
-    }];
+    # services.flatpak.remotes = [{
+    #   name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+    # }];
 
     services.flatpak.packages = [
       "dev.vencord.Vesktop"
