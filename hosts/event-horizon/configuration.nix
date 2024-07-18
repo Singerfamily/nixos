493c40e34a58887c641b7c7fe7a661e57245918c
LeaderbotX400 <eric@singerfamily.ca>
2024-07-18 13:00:04 -0600

    feat: Enable Steam and NVIDIA graphics for improved gaming performance

--- hosts/event-horizon/configuration.nix
+++ hosts/event-horizon/configuration.nix
@@ -10,9 +10,9 @@
       ./hardware-configuration.nix
     ];
 
-  config = {
-    nvidia.enable = true;
-  };
+  nvidia.enable = true;
+
+  steam.enable = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
