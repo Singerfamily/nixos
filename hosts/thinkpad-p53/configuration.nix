f52dfa6bafe9d1f19014b8333c5a05e7c6c6ac67
LeaderbotX400 <eric@singerfamily.ca>
2024-07-19 13:08:06 -0600

    chore: Enable fprintd and bolt services in configuration.nix
    This commit enables the fprintd and bolt services in the configuration.nix file for the ThinkPad P53 host. The fprintd service allows for fingerprint authentication, while the bolt service provides support for Thunderbolt devices. This change enhances the functionality and security of the system.

--- hosts/thinkpad-p53/configuration.nix
+++ hosts/thinkpad-p53/configuration.nix
@@ -11,15 +11,16 @@
     ];
 
   secureboot.enable = true;
-
   bluetooth.enable = true;
-
   nvidia = {
     enable = true;
     prime = true;
   };
 
-  services.fprintd.enable = true;
+  services = {
+    fprintd.enable = true;
+    hardware.bolt.enable = true;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -28,5 +29,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
