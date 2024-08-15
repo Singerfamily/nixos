6a65068240baf39569eb8e0b4420247b289003a7
LeaderbotX400 <eric@singerfamily.ca>
2024-08-15 16:28:09 -0600

    chore: Update systemPackages in default.nix

--- modules/nixos/core/default.nix
+++ modules/nixos/core/default.nix
@@ -1,33 +1,24 @@
-{pkgs, ...}: 
+{ pkgs, ... }:
 
 {
-    imports = [
-        ./nix
-        ./boot
-        ./networking
-        ./locale.nix
-        ./pipewire.nix
-        ./fonts.nix
-    ];
+  imports =
+    [ ./nix ./boot ./networking ./locale.nix ./pipewire.nix ./fonts.nix ];
 
-    programs = {
-        git.enable = true;
-        zsh.enable = true;
-        direnv.enable = true;
-        kdeconnect = {
-            enable = true;
-            # package = pkgs.kdePackages.kdeconnect-kde;
-        };
-    };
+  programs = {
+    git.enable = true;
+    zsh.enable = true;
+    direnv.enable = true;
+    kdeconnect.enable = true;
+  };
 
-    environment.systemPackages = with pkgs; [
-        gh
-        niv
-        krdc
-	    vscode
-        tpm2-tss
-        binutils
-        librewolf
-	    microsoft-edge-dev
-    ];
+  environment.systemPackages = with pkgs; [
+    gh
+    niv
+    krdc
+    vscode
+    tpm2-tss
+    binutils
+    librewolf
+    microsoft-edge-dev
+  ];
 }
