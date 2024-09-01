29d87dc7c7b2643914d061f4cee4e28e613b53a5
clintsinger <clint@singerfamily.ca>
2024-08-31 18:16:01 -0600

    feat: setup nuc

---
+++ hosts/nucos/configuration.nix
@@ -0,0 +1,28 @@
+{pkgs, ...}: {
+  imports = [ ./hardware-configuration.nix ];
+
+  drivers = {
+    nvidia.enable = true;
+    intel.enable = true;
+  };
+
+  programs = {
+    # spotify.enable = true;
+    # steam.enable = true;
+    # thunderbird.enable = true;
+    firefox.enable = true;
+    nix-ld.enable = true;
+  };
+
+  services = {
+    flatpak.enable = true;
+    # onedrive.enable = true;
+    # davfs2.enable = true;
+  };
+
+  virtualisation = {
+    qemu.enable = true;
+    docker.enable = true;
+    podman.enable = true;
+  };
+}
