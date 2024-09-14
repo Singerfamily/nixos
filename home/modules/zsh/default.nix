f3947e262909e52c5b7b42022be887bc07e32203
LeaderbotX400 <eric@singerfamily.ca>
2024-09-14 10:05:55 -0600

    feat: Add powerlevel10k and zsh configuration options
    This commit adds the powerlevel10k plugin and zsh configuration options to the default.nix file in the zsh module. It enables the powerlevel10k theme, sets up shell aliases, and includes plugins for git and kubectl. Additionally, it adds the necessary configuration for autosuggestions and syntax highlighting. This update improves the overall functionality and customization options for the zsh shell.

--- home/modules/zsh/default.nix
+++ home/modules/zsh/default.nix
@@ -1,63 +1,72 @@
-{ config, lib, pkgs, homeModules, ... }: let
+{
+  config,
+  lib,
+  pkgs,
+  homeModules,
+  ...
+}:
+let
   cfg = config.programs.zsh;
-in {
-  
-    home.file.".p10k.zsh".source = "${homeModules}/zsh/p10k.zsh";
+in
+{
 
-    programs = {
-      zoxide = {
-        enable = true;
-        enableZshIntegration = true;
-        options = [
-          "--cmd cd"
-        ];
+  home.file.".p10k.zsh".source = "${homeModules}/zsh/p10k.zsh";
+
+  programs = {
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      options = [
+        "--cmd cd"
+      ];
+    };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    zsh = {
+      enable = true;
+
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      initExtra = ''
+        cp ~/.zsh_history ~/.zsh_history.bak
+        strings ~/.zsh_history.bak > ~/.zsh_history
+        fc -R ~/.zsh_history
+
+        export XDG_DATA_HOME="$HOME/.local/share"
+        eval "$(tailscale completion zsh)"
+      '';
+
+      shellAliases = {
+        ll = "ls -l";
+        la = "ls -la";
+        update = "sudo nixos-rebuild switch --flake $HOME";
       };
 
-      fzf = {
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+          file = "powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = lib.cleanSource "${homeModules}/zsh";
+          file = "p10k.zsh";
+        }
+      ];
+
+      oh-my-zsh = {
         enable = true;
-        enableZshIntegration = true;
-      };
-
-      zsh = {
-        enable = true;
-
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
-
-        initExtra = ''
-          cp ~/.zsh_history ~/.zsh_history.bak
-          strings ~/.zsh_history.bak > ~/.zsh_history
-          fc -R ~/.zsh_history
-
-          export XDG_DATA_HOME="$HOME/.local/share"
-        '';
-
-        shellAliases = {
-          ll = "ls -l";
-          la = "ls -la";
-          update = "sudo nixos-rebuild switch --flake $HOME";
-        };
-
         plugins = [
-          {
-            name = "powerlevel10k";
-            src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-            file = "powerlevel10k.zsh-theme";
-          }
-          {
-            name = "powerlevel10k-config";
-            src = lib.cleanSource "${homeModules}/zsh";
-            file = "p10k.zsh";
-          }
+          "git"
+          "kubectl"
         ];
-
-        oh-my-zsh = {
-          enable = true;
-          plugins = [
-            "git"
-            "kubectl"
-          ];
-        };
       };
     };
+  };
 }
