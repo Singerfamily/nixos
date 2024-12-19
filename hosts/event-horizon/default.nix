331e0277b5fc36b9b06e5223234e06662f4d4ec2
LeaderbotX400 <eric@singerfamily.ca>
2024-12-19 10:47:28 -0700

    feat(nixos): add username parameter and configure udev rules for OBS video device

--- hosts/event-horizon/default.nix
+++ hosts/event-horizon/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 {
   imports = [ ./hardware-configuration.nix ];
 
@@ -49,10 +49,23 @@
     variables = {
       NIXOS_OZONE_WL = "1";
     };
+
+    systemPackages = with pkgs; [
+      dbeaver-bin
+    ];
   };
 
   fileSystems."/mnt/media" = {
     device = "10.0.0.3:/mnt/stuff/media";
     fsType = "nfs";
   };
+
+  services.udev.extraRules =
+    let
+      obsPath = "/home/${username}/obs";
+    in
+    ''
+      ACTION=="add", SUBSYSTEM=="video4linux", KERNEL=="video0", RUN+="${pkgs.docker}/bin/docker compose -f ${obsPath}/docker-compose.yml up -d"
+      ACTION=="remove", SUBSYSTEM=="video4linux", KERNEL=="video0", RUN+="${pkgs.docker}/bin/docker compose -f ${obsPath}/docker-compose.yml down"
+    '';
 }
