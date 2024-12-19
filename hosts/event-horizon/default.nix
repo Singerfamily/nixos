fa24b3e190c3fd36c1451ec9a0db2bdbdf6713a9
LeaderbotX400 <eric@singerfamily.ca>
2024-12-19 11:09:30 -0700

    refactor(nixos): remove udev rules for OBS video device from default.nix

--- hosts/event-horizon/default.nix
+++ hosts/event-horizon/default.nix
@@ -59,13 +59,4 @@
     device = "10.0.0.3:/mnt/stuff/media";
     fsType = "nfs";
   };
-
-  services.udev.extraRules =
-    let
-      obsPath = "/home/${username}/obs";
-    in
-    ''
-      ACTION=="add", SUBSYSTEM=="video4linux", KERNEL=="video0", RUN+="${pkgs.docker}/bin/docker compose -f ${obsPath}/docker-compose.yml up -d"
-      ACTION=="remove", SUBSYSTEM=="video4linux", KERNEL=="video0", RUN+="${pkgs.docker}/bin/docker compose -f ${obsPath}/docker-compose.yml down"
-    '';
 }
