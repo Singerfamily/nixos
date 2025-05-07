a6c37e005691a14416e40b23ad320328c7e11781
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-05-07 17:06:46 -0600

    feat(nixos): add NixOS modules for dnscrypt-proxy2, QEMU/KVM, sops, PipeWire, and user shell configuration

---
+++ modules/nixos/sops/default.nix
@@ -0,0 +1,17 @@
+# INFO: NixOS sops-nix module.
+
+{
+  config,
+  lib,
+  ...
+}:
+
+with lib;
+{
+  config = {
+    sops = {
+      defaultSopsFile = mkDefault ../../../lib/secrets.yaml;
+      age.keyFile = "${config.home-manager.users.${lib.aeon.user}.xdg.configHome}/sops/age/keys.txt";
+    };
+  };
+}
---
+++ modules/nixos/users/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+with lib;
+{
+  config = mkIf (config.home-manager.users |> builtins.hasAttr "${aeon.user}") {
+    users.users = {
+      ${aeon.user} = {
+        hashedPasswordFile = config.sops.secrets."passwords/user".path;
+        openssh.authorizedKeys.keys = aeon.pubKeys;
+        extraGroups =
+          [
+            "wheel"
+            "video"
+            "audio"
+            "input"
+          ]
+          ++ (
+            [
+              "networkmanager"
+              "docker"
+              "podman"
+              "git"
+              "libvirtd"
+            ]
+            |> builtins.filter (G: builtins.hasAttr G config.users.groups)
+          );
+      };
+    };
+
+    sops.secrets."passwords/user".neededForUsers = true;
+  };
+}
