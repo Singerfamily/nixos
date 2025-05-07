f525a3c1765bf11d69c311ab5b066a2c4b3db80b
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-05-07 17:01:48 -0600

    refactor(nixos): improve formatting and readability of networking module in default.nix

--- modules/nixos/net/default.nix
+++ modules/nixos/net/default.nix
@@ -1,62 +1,69 @@
 # INFO: NixOS networking module.
 
 {
-    config,
-    lib,
-    pkgs,
-    ...
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 
-with lib; {
-    options.aeon.net = {
-        # Whether or not to use NetworkManager.
-        networkmanager = mkOption {
-            type = types.bool;
-            default = true;
-        };
-
-        # Whether to open port 9 for WakeOnLAN.
-        allowWOL = mkOption {
-            type = types.bool;
-            default = false;
-        };
+with lib;
+{
+  options.aeon.net = {
+    # Whether or not to use NetworkManager.
+    networkmanager = mkOption {
+      type = types.bool;
+      default = true;
     };
 
-    config = let
-        inherit (config.aeon.net) networkmanager allowWOL;
-        ports.open = if allowWOL then [ 9 ] else [ ];
-    in {
-        networking = {
-            useDHCP = lib.mkDefault true;
-            networkmanager.enable = networkmanager;
-            firewall = {
-                enable = true;
-                allowedTCPPorts = ports.open;
-                allowedUDPPorts = ports.open;
-            };
-        };
+    # Whether to open port 9 for WakeOnLAN.
+    allowWOL = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
 
-        environment = {
-            systemPackages = with pkgs; [
-                aeon.siren # My Wake-on-LAN tool written in Rust.
-                dig
-                ethtool
-                iperf3
-                wakeonlan
-            ];
-
-            etc."nix/open_ports.json".text = let
-                inherit (config.networking.firewall)
-                    allowedTCPPorts
-                    allowedTCPPortRanges
-                    allowedUDPPorts
-                    allowedUDPPortRanges
-                    ;
-                rangesToList = ranges: ranges |> builtins.map (range: "${toString range.from}-${toString range.to}");
-            in /* json */ builtins.toJSON {
-                tcp = allowedTCPPorts ++ rangesToList allowedTCPPortRanges;
-                udp = allowedUDPPorts ++ rangesToList allowedUDPPortRanges;
-            };
+  config =
+    let
+      inherit (config.aeon.net) networkmanager allowWOL;
+      ports.open = if allowWOL then [ 9 ] else [ ];
+    in
+    {
+      networking = {
+        useDHCP = lib.mkDefault true;
+        networkmanager.enable = networkmanager;
+        firewall = {
+          enable = true;
+          allowedTCPPorts = ports.open;
+          allowedUDPPorts = ports.open;
         };
+      };
+
+      environment = {
+        systemPackages = with pkgs; [
+          aeon.siren # My Wake-on-LAN tool written in Rust.
+          dig
+          ethtool
+          iperf3
+          wakeonlan
+        ];
+
+        etc."nix/open_ports.json".text =
+          let
+            inherit (config.networking.firewall)
+              allowedTCPPorts
+              allowedTCPPortRanges
+              allowedUDPPorts
+              allowedUDPPortRanges
+              ;
+            rangesToList =
+              ranges: ranges |> builtins.map (range: "${toString range.from}-${toString range.to}");
+          in
+          # json
+          builtins.toJSON {
+            tcp = allowedTCPPorts ++ rangesToList allowedTCPPortRanges;
+            udp = allowedUDPPorts ++ rangesToList allowedUDPPortRanges;
+          };
+      };
     };
 }
