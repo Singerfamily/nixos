73f155d0afd061d9961219b3f7808c61abd5e22b
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-05 13:37:28 -0600

    refactor(nixos): clean up commented-out code and adjust Flatpak configuration in sandbox

--- systems/x86_64-linux/event-horizon/default.nix
+++ systems/x86_64-linux/event-horizon/default.nix
@@ -1,21 +1,6 @@
 {
-  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
-  # as well as the libraries available from your flake's inputs.
   lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
   pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-
-  # Additional metadata is provided by Snowfall Lib.
-  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
-  system, # The system architecture for this host (eg. `x86_64-linux`).
-  target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format, # A normalized name for the system target (eg. `iso`).
-  virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
-
-  # All other arguments come from the system system.
   config,
   ...
 }:
@@ -27,6 +12,7 @@
       # quiet = true; # Enable Plymouth and reduce TTY verbosity
     };
   };
+
   # snowfall.hardware.adb.enable = true;
 
   services = {
@@ -84,7 +70,7 @@
       disk = {
         main = {
           type = "disk";
-          device = "/dev/sda";
+          device = "/dev/nvme0n1";
           content = {
             type = "gpt";
             partitions = {
@@ -150,25 +136,14 @@
     };
 
   boot.initrd.availableKernelModules = [
-
-    "ata_piix"
-    "uhci_hcd"
-    "virtio_pci"
-    "virtio_scsi"
+    "xhci_pci"
+    "ahci"
+    "nvme"
+    "usbhid"
     "sd_mod"
-    "sr_mod"
-
-    # "xhci_pci"
-    # "ahci"
-    # "nvme"
-    # "usbhid"
-    # "usb_storage"
-    # "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [
-    # "kvm-intel"
-  ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   system.stateVersion = "24.11";
