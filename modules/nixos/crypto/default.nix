daff1cbe38439b37ba8f810eadb3c5be1a3732ef
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-05 13:39:19 -0600

    feat(nixos): add NixOS cryptography module with configuration options

---
+++ modules/nixos/crypto/default.nix
@@ -0,0 +1,36 @@
+# INFO: NixOS cryptography module.
+
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+{
+  options.snowfall.crypto = {
+    enable = mkOption {
+      type = with types; bool;
+      default = true;
+      description = "Whether to enable cryptography support";
+    };
+  };
+
+  config =
+    let
+      inherit (config.snowfall.crypto)
+        enable
+        ;
+    in
+    mkIf enable {
+      programs.gnupg.agent.enable = true;
+      environment.systemPackages = with pkgs; [
+        sops # An editor of encrypted files (for `sops-nix`)
+        ssh-to-age # Convert ED25519 SSH private keys to age keys.
+        age # Modern encryption tool with small explicit keys. (Go version)
+        rage # Modern encryption tool with small explicit keys. (Rust version)
+        cryptsetup # LUKS for dm-crypt.
+      ];
+    };
+}
