eebc65693003766bef9eeb551fb2635f98369506
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-05 20:58:49 -0600

    feat(nixos): add NixOS TPM module for secure boot configuration

---
+++ modules/nixos/hardware/tpm/default.nix
@@ -0,0 +1,42 @@
+# INFO: NixOS TPM module.
+
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+{
+  options.snowfall.tpm = {
+    # Whether to use TPM2 for secure boot.
+    enable = mkOption {
+      type = with types; bool;
+      default = false;
+    };
+  };
+
+  config =
+    let
+      inherit (config.snowfall.tpm) enable;
+    in
+    mkMerge [
+      # TPM2: common options.
+      (mkIf enable {
+        boot.initrd = {
+          systemd = {
+            enable = true; # For auto unlock
+            tpm2.enable = true;
+          };
+          kernelModules = [ "tpm_crb" ];
+          availableKernelModules = [ "tpm_crb" ];
+        };
+
+        security.tpm2 = {
+          enable = true;
+          tctiEnvironment.enable = true;
+        };
+      })
+    ];
+}
