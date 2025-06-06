a60f9e16f66bb24eea60fff51d38f7f6a54abf10
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-05 21:49:52 -0600

    feat(nixos): add Hyprland Home-manager module for desktop configuration

---
+++ modules/home/desktop/hyprland/default.nix
@@ -0,0 +1,97 @@
+# INFO: Hyprland Home-manager module.
+
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+
+with lib;
+{
+  # HACK: Here, because importing in flake.nix does not work.
+  imports = with inputs; [ hyprland.homeManagerModules.default ];
+
+  options.snowfall.desktop.hyprland = {
+    enable = mkOption {
+      description = "Whether to enable and configure Hyprland";
+      type = with types; bool;
+      default = false;
+    };
+
+    # NOTE: Using `"nixpkgs"` will make Nix use the Nixpkgs-provided version of Hyprland.
+    # Otherwise, Hyprland will come from flake's inputs (see flake.nix). That might cause
+    # some extra building from source, however gives more control over versions of stuff.
+    source = mkOption {
+      type =
+        with types;
+        enum [
+          "nixpkgs"
+          "git"
+        ];
+      default = "git";
+    };
+  };
+
+  config =
+    let
+      inherit (config.snowfall.desktop.hyprland)
+        enable
+        source
+        ;
+    in
+    mkIf enable {
+      wayland.windowManager.hyprland = {
+        enable = true;
+        package =
+          if (source == "nixpkgs") then pkgs.hyprland else inputs.hyprland.packages.${pkgs.system}.default;
+
+        settings =
+          let
+            MOD = "SUPER";
+            workspaceCount = 10;
+          in
+          {
+            bind = builtins.concatLists [
+              # General binds.
+              [
+                "${MOD} CTRL SHIFT, E, exit"
+              ]
+
+              # Generate bindings for switching or moving active window to a workspace.
+              (
+                workspaceCount
+                |> builtins.genList (
+                  _id:
+                  let
+                    id = toString (_id + 1);
+                  in
+                  [
+                    "${MOD},       ${id}, workspace,       ${id}"
+                    "${MOD} SHIFT, ${id}, movetoworkspace, ${id}"
+                  ]
+                )
+                |> builtins.concatLists
+              )
+            ];
+
+            exec-once = [
+              "${pkgs.alacritty}/bin/alacritty"
+            ];
+          };
+
+        plugins =
+          if (source == "nixpkgs") then
+            [
+              pkgs.hyprlandPlugins.hy3
+              pkgs.hyprlandPlugins.borders-plus-plus
+            ]
+          else
+            [
+              inputs.hyprland-hy3.packages.${pkgs.system}.hy3
+              inputs.hyprland-plugins.packages.${pkgs.system}.borders-plus-plus
+            ];
+      };
+    };
+}
