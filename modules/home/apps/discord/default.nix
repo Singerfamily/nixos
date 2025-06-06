e690d0292db3d7c9af4a63458ea2ae95dbb1480e
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-05 21:49:27 -0600

    feat(nixos): add Discord Home-manager module with vencord support

---
+++ modules/home/apps/discord/default.nix
@@ -0,0 +1,46 @@
+# INFO: Discord Home-manager module.
+
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+{
+  options.snowfall.apps.discord = {
+    enable = mkOption {
+      type = with types; bool;
+      default = false;
+      description = "Whether to enable Discord";
+    };
+
+    app = mkOption {
+      type = with types; enum [ "vencord" ];
+      default = "vencord";
+      description = "What Discord client to use";
+    };
+  };
+
+  config =
+    let
+      inherit (config.snowfall.apps.discord)
+        enable
+        app
+        ;
+    in
+    mkIf enable (mkMerge [
+      (mkIf (app == "vencord") {
+        home = {
+          packages = with pkgs; [ vesktop ];
+          persistence."${lib.snowfall.persist}/home/${lib.snowfall.user}" = {
+            directories = [
+              ".config/vesktop"
+              ".config/VencordDesktop"
+            ];
+          };
+        };
+      })
+    ]);
+}
