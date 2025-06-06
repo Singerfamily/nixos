ed1469fc08922c22738633835c37616fb8ffa381
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-05 21:49:48 -0600

    feat(nixos): add Shell Home-manager module for configurable default shell

---
+++ modules/home/cli/shell/default.nix
@@ -0,0 +1,22 @@
+# INFO: Shell Home-manager module.
+
+{
+  lib,
+  ...
+}:
+
+with lib;
+{
+  options.aeon.cli.shell = {
+    default = mkOption {
+      description = "Which shell to use as default";
+      type = types.enum [
+        "bash" # TODO: Implement shell/<shell>.nix modules.
+        "zsh"
+        "fish"
+        "nushell"
+      ];
+      default = "zsh";
+    };
+  };
+}
