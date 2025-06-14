22d0685d50636066b0413092eb9afa0f09e4402f
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-13 22:05:59 -0600

    fix(nixos): correct typo in .p10k.zsh file reference in default.nix

---
+++ modules/nixos/fonts/default.nix
@@ -0,0 +1,26 @@
+{pkgs, ...}: {
+   fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      nerd-fonts.monaspace
+      # monaspace
+      # fira-code
+      # (nerd-fonts.override { fonts = [ 
+      #   "Monaspace"
+      #   "FiraCode"
+      #   # "JetBrainsMono"
+      #   # "DroidSansMono" 
+      #   ]; 
+      # })
+      corefonts
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        serif = [  "Monaspace" ];
+        sansSerif = [ "Monaspace" ];
+        monospace = [ "Monaspace" ];
+      };
+    };
+  };
+}
