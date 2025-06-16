b97b0bbe69c0356edf20c7c5c9e57c2faf6cd8aa
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-16 17:05:43 -0600

    refactor(default.nix): restructure dotnet options for clarity

--- modules/home/dev/dotnet/default.nix
+++ modules/home/dev/dotnet/default.nix
@@ -9,10 +9,19 @@
 
 with lib;
 {
-  options.snowfall.dev.dotnet = {
-    enable = mkOption {
-      type = types.bool;
-      default = false;
+  options.snowfall.dev = {
+    dotnet = {
+      rider = {
+        enable = mkOption {
+          type = types.bool;
+          default = true;
+          description = "Enable JetBrains Rider for .NET development.";
+        };
+      };
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+      };
     };
   };
 
@@ -20,13 +29,13 @@ with lib;
     let
       inherit (config.snowfall.dev.dotnet)
         enable
+        rider
         ;
     in
     mkIf enable {
       home.packages = with pkgs; [
-        jetbrains.rider
-        dotnet-sdk_9
-        dotnet-ef
+        (mkIf rider.enable jetbrains.rider)
+        dotnetCorePackages.dotnet_9.sdk
       ];
     };
 }
