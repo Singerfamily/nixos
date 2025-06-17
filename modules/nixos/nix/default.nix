84d04fa1c11ec53af9418e074e7193c50739a93e
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-16 19:24:25 -0600

    fix: Update formatting and structure of nix settings in default.nix

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -1,25 +1,42 @@
-{ ... }:
 {
-  nix = {
-    settings = {
-      trusted-users = [
-        "root"
-        "@wheel"
-      ];
-      experimental-features = [
-        "ca-derivations"
-      ];
-      system-features = [
-        "kvm"
-        "big-parallel"
-        "nixos-test"
-      ];
-      auto-optimise-store = true;
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 14d";
-    };
+  config,
+  inputs,
+  lib,
+  ...
+}:
+{
+  # Register flake inputs for nix commands
+  nix.registry = lib.mapAttrs (_: flake: { inherit flake; }) (
+    lib.filterAttrs (_: lib.isType "flake") inputs
+  );
+
+  nix.settings = {
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
+    experimental-features = [
+      "ca-derivations"
+    ];
+    system-features = [
+      "kvm"
+      "big-parallel"
+      "nixos-test"
+    ];
+    auto-optimise-store = true;
   };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
+  # Add inputs to legacy channels
+  nix.nixPath = [ "/etc/nix/path" ];
+
+  environment.etc = lib.mapAttrs' (name: value: {
+    name = "nix/path/${name}";
+    value.source = value.flake;
+  }) config.nix.registry;
 }
