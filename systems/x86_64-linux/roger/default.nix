163dfed549bfd61f36af79119afced51b98823ca
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-23 17:42:40 -0600

    fix: Add platformio-core and openocd to system packages in default.nix

--- systems/x86_64-linux/roger/default.nix
+++ systems/x86_64-linux/roger/default.nix
@@ -32,75 +32,148 @@
         };
       };
     };
-    
+
     qemu.enable = true;
   };
 
   programs = {
-    # kdeconnect.enable = true;
-    # vscode.enable = true;
+    firefox.enable = true;
   };
 
   services = {
     vscode-server.enable = true;
+    udev = {
+      packages = [
+        pkgs.platformio-core
+        pkgs.openocd
+      ];
+      extraRules = ''
+        # udev rule for ST-LINK/V2 
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE:="0666", SYMLINK+="stlinkv2_%n"
+      '';
+    };
+    openssh = {
+      enable = true;
+      openFirewall = true;
+      # settings = {
+      #   PermitRootLogin = "no";
+      #   PasswordAuthentication = "no";
+      #   PubkeyAuthentication = "yes";
+      #   ChallengeResponseAuthentication = "no";
+      #   UsePAM = "yes";
+      #   X11Forwarding = "yes";
+      #   AllowAgentForwarding = "yes";
+      #   AllowTcpForwarding = "yes";
+      #   PrintMotd = "no";
+      #   PrintLastLog = "yes";
+      # };
+    };
+    nginx = {
+      enable = true;
+
+      # Use recommended settings
+      recommendedZstdSettings = true;
+      # recommendedBrotliSettings = true;
+      # recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+
+      # other Nginx options
+      virtualHosts."nucos" =
+        let
+          tls-cert =
+            {
+              alt ? [ ],
+            }:
+            (pkgs.runCommand "selfSignedCert" { buildInputs = [ pkgs.openssl ]; } ''
+              mkdir -p $out
+              openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -days 365 -nodes \
+                -keyout $out/cert.key -out $out/cert.crt \
+                -subj "/CN=localhost" -addext "subjectAltName=DNS:localhost,${
+                  builtins.concatStringsSep "," ([ "IP:127.0.0.1" ] ++ alt)
+                }"
+            '');
+          cert = tls-cert { alt = [ "IP:192.168.1.131" ]; };
+        in
+        {
+          addSSL = true;
+
+          sslCertificate = "${cert}/cert.crt";
+          sslCertificateKey = "${cert}/cert.key";
+
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:3000";
+            proxyWebsockets = true; # needed if you need to use WebSocket
+          };
+          locations."/obs-ws" = {
+            proxyPass = "http://127.0.0.1:4455";
+            proxyWebsockets = true; # needed if you need to use WebSocket
+          };
+        };
+    };
   };
 
   environment = {
     variables = {
       NIXOS_OZONE_WL = "1";
+      OPENOCD_PATH = "${pkgs.openocd}";
+      OPENOCD_SCRIPTS_PATH = "$OPENOCD_PATH/share/openocd/scripts";
     };
     systemPackages = with pkgs; [
-      # nixd
-      # binutils
-      # htop
-      # nixfmt-rfc-style
-      # tpm2-tss
-      # nvtopPackages.full
-      # usbutils
-      # ethtool
-      # vscode
+      obs-studio
+      talosctl
+      python3
+      python312Packages.pyudev
 
-      # deno
+      platformio-core
+      (pkgs.python3.withPackages (ps: with ps; [ platformio ]))
+
+      stlink
+      openocd
+      gdb
+      cmake
+      gcc
+      stm32cubemx
+      gcc-arm-embedded-13
     ];
   };
 
-  services.openssh = {
-    enable = true;
-    openFirewall = true;
-    # settings = {
-    #   PermitRootLogin = "no";
-    #   PasswordAuthentication = "no";
-    #   PubkeyAuthentication = "yes";
-    #   ChallengeResponseAuthentication = "no";
-    #   UsePAM = "yes";
-    #   X11Forwarding = "yes";
-    #   AllowAgentForwarding = "yes";
-    #   AllowTcpForwarding = "yes";
-    #   PrintMotd = "no";
-    #   PrintLastLog = "yes";
-    # };
-  };
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "thunderbolt"
+    "nvme"
+    "usb_storage"
+    "usbhid"
+    "sd_mod"
+    "rtsx_pci_sdmmc"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/8936eed1-ba38-47d9-aa87-819a2ac1bb9d";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/8936eed1-ba38-47d9-aa87-819a2ac1bb9d";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/013A-5554";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/013A-5554";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
 
   swapDevices = [ ];
 
-
-#   networking.hostId = "edc49e33";
+  networking.hostId = "007f0200";
 
   system.stateVersion = "24.11";
 }
