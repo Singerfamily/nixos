e94e0d7b090c526d89246eb4d37308e5acd28d60
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-06-26 09:33:24 -0600

    fix: Change default SSH enable option to true in default.nix

--- modules/nixos/network/ssh/default.nix
+++ modules/nixos/network/ssh/default.nix
@@ -12,7 +12,7 @@ with lib;
   options.snowfall.net.ssh = {
     enable = mkOption {
       type = types.bool;
-      default = false;
+      default = true;
       description = "Whether to enable SSH";
     };
 
@@ -31,17 +31,27 @@ with lib;
         ;
     in
     mkMerge [
-      # {
-      #   assertions = [
-      #     {
-      #       assertion = (server != null);
-      #       message = ''
-      #         Please state explicitly whether to run an SSH server on the machine.
-      #         To do so, set the `snowfall.net.ssh.server` option to `true` or `false`.
-      #       '';
-      #     }
-      #   ];
-      # }
+      {
+
+        services.openssh.hostKeys = let 
+          inherit (config.networking) hostName;
+        in [
+          { 
+            path = config.sops.secrets."ssh/${hostName}".path;
+            type = "ed25519";
+          }
+        ];
+
+        #   assertions = [
+        #     {
+        #       assertion = (server != null);
+        #       message = ''
+        #         Please state explicitly whether to run an SSH server on the machine.
+        #         To do so, set the `snowfall.net.ssh.server` option to `true` or `false`.
+        #       '';
+        #     }
+        #   ];
+      }
 
       (mkIf (enable && server != null) {
         services.openssh = {
