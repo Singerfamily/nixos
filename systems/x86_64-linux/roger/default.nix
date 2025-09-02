17363bc0a9fb78a99bc8ecc20d54a6318f3088a6
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-09-02 17:35:21 -0600

    fix: Comment out udev and nginx service configurations in default.nix

--- systems/x86_64-linux/roger/default.nix
+++ systems/x86_64-linux/roger/default.nix
@@ -44,60 +44,60 @@
 
   services = {
     vscode-server.enable = true;
-    udev = {
-      packages = [
-        pkgs.platformio-core
-        pkgs.openocd
-      ];
-      extraRules = ''
-        # udev rule for ST-LINK/V2 
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE:="0666", SYMLINK+="stlinkv2_%n"
-      '';
-    };
-    nginx = {
-      enable = true;
+    # udev = {
+    #   packages = [
+    #     pkgs.platformio-core
+    #     pkgs.openocd
+    #   ];
+    #   extraRules = ''
+    #     # udev rule for ST-LINK/V2 
+    #     SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE:="0666", SYMLINK+="stlinkv2_%n"
+    #   '';
+    # };
+    # nginx = {
+    #   enable = true;
 
-      # Use recommended settings
-      recommendedZstdSettings = true;
-      # recommendedBrotliSettings = true;
-      # recommendedGzipSettings = true;
-      recommendedOptimisation = true;
-      recommendedProxySettings = true;
-      recommendedTlsSettings = true;
+    #   # Use recommended settings
+    #   recommendedZstdSettings = true;
+    #   # recommendedBrotliSettings = true;
+    #   # recommendedGzipSettings = true;
+    #   recommendedOptimisation = true;
+    #   recommendedProxySettings = true;
+    #   recommendedTlsSettings = true;
 
-      # other Nginx options
-      virtualHosts."nucos" =
-        let
-          tls-cert =
-            {
-              alt ? [ ],
-            }:
-            (pkgs.runCommand "selfSignedCert" { buildInputs = [ pkgs.openssl ]; } ''
-              mkdir -p $out
-              openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -days 365 -nodes \
-                -keyout $out/cert.key -out $out/cert.crt \
-                -subj "/CN=localhost" -addext "subjectAltName=DNS:localhost,${
-                  builtins.concatStringsSep "," ([ "IP:127.0.0.1" ] ++ alt)
-                }"
-            '');
-          cert = tls-cert { alt = [ "IP:192.168.1.131" ]; };
-        in
-        {
-          addSSL = true;
+    #   # other Nginx options
+    #   virtualHosts."nucos" =
+    #     let
+    #       tls-cert =
+    #         {
+    #           alt ? [ ],
+    #         }:
+    #         (pkgs.runCommand "selfSignedCert" { buildInputs = [ pkgs.openssl ]; } ''
+    #           mkdir -p $out
+    #           openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -days 365 -nodes \
+    #             -keyout $out/cert.key -out $out/cert.crt \
+    #             -subj "/CN=localhost" -addext "subjectAltName=DNS:localhost,${
+    #               builtins.concatStringsSep "," ([ "IP:127.0.0.1" ] ++ alt)
+    #             }"
+    #         '');
+    #       cert = tls-cert { alt = [ "IP:192.168.1.131" ]; };
+    #     in
+    #     {
+    #       addSSL = true;
 
-          sslCertificate = "${cert}/cert.crt";
-          sslCertificateKey = "${cert}/cert.key";
+    #       sslCertificate = "${cert}/cert.crt";
+    #       sslCertificateKey = "${cert}/cert.key";
 
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:3000";
-            proxyWebsockets = true; # needed if you need to use WebSocket
-          };
-          locations."/obs-ws" = {
-            proxyPass = "http://127.0.0.1:4455";
-            proxyWebsockets = true; # needed if you need to use WebSocket
-          };
-        };
-    };
+    #       locations."/" = {
+    #         proxyPass = "http://127.0.0.1:3000";
+    #         proxyWebsockets = true; # needed if you need to use WebSocket
+    #       };
+    #       locations."/obs-ws" = {
+    #         proxyPass = "http://127.0.0.1:4455";
+    #         proxyWebsockets = true; # needed if you need to use WebSocket
+    #       };
+    #     };
+    # };
   };
 
   environment = {
