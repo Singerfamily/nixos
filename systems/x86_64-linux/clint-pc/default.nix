5656347a42f9b69fe5cf8732aa34c270fdecb18b
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-09-02 18:34:46 -0600

    Add clint pc to sops

--- systems/x86_64-linux/clint-pc/default.nix
+++ systems/x86_64-linux/clint-pc/default.nix
@@ -129,74 +129,71 @@
   #   80
   #   443
   # ];
-  disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/disk/by-id/md-uuid-4b21de7f:f29ca007:298b5060:1dacafb5";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "128M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ]; # Override existing partition
-                # Subvolumes must set a mountpoint in order to be mounted,
-                # unless their parent is mounted
-                subvolumes = {
-                  # Subvolume name is different from mountpoint
-                  "/rootfs" = {
-                    mountpoint = "/";
-                  };
-                  # Subvolume name is the same as the mountpoint
-                  "/home" = {
-                    mountOptions = [ "compress=zstd" ];
-                    mountpoint = "/home";
-                  };
-                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
-                  "/home/user" = { };
-                  # Parent is not mounted so the mountpoint must be set
-                  "/nix" = {
-                    mountOptions = [
-                      "compress=zstd"
-                      "noatime"
-                    ];
-                    mountpoint = "/nix";
-                  };
-                  # This subvolume will be created but not mounted
-                  "/test" = { };
-                  # Subvolume for the swapfile
-                  "/swap" = {
-                    mountpoint = "/.swapvol";
-                    swap = {
-                      swapfile.size = "20M";
-                      swapfile2.size = "20M";
-                      swapfile2.path = "rel-path";
-                    };
-                  };
-                };
 
-                mountpoint = "/partition-root";
-                swap = {
-                  swapfile = {
-                    size = "20M";
+  disko.devices =
+    let
+      inherit (config.networking) hostName;
+    in
+    {
+      disk = {
+        main = {
+          type = "disk";
+          device = "/dev/nvme0n1";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "512M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+              luks = {
+                size = "100%";
+                content = {
+                  type = "luks";
+                  name = "${lib.toUpper hostName}_LUKS";
+                  # disable settings.keyFile if you want to use interactive password entry
+                  #passwordFile = "/tmp/secret.key"; # Interactive
+                  settings = {
+                    allowDiscards = true;
+                    # keyFile = "/tmp/secret.key";
                   };
-                  swapfile1 = {
-                    size = "20M";
+                  # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [ "-f" ];
+                    subvolumes = {
+                      "/root" = {
+                        mountpoint = "/";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "/home" = {
+                        mountpoint = "/home";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "/nix" = {
+                        mountpoint = "/nix";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      "/swap" = {
+                        mountpoint = "/.swapvol";
+                        swap.swapfile.size = "32G";
+                      };
+                    };
                   };
                 };
               };
@@ -205,7 +202,6 @@
         };
       };
     };
-  };
 
   # DANGER - Do not Modify Below!
 
