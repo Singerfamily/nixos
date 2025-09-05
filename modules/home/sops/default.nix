5668f59e1cf552d5eac75946b16894ee5abab23e
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-09-05 12:33:27 -0600

    Disable sops for now

--- modules/home/sops/default.nix
+++ modules/home/sops/default.nix
@@ -14,21 +14,21 @@ with lib;
   # HACK: For some reason, importing it in flake.nix fails, but works here...
   # imports = with inputs; [ sops-nix.homeManagerModules.sops ];
 
-  options.snowfall.sops = {
-    # Whether to decrypt and use the Age key from lib/secrets.yaml.
-    inheritKeyFile = mkOption {
-      type = with types; bool;
-      default = false;
-    };
-  };
+  # options.snowfall.sops = {
+  #   # Whether to decrypt and use the Age key from lib/secrets.yaml.
+  #   inheritKeyFile = mkOption {
+  #     type = with types; bool;
+  #     default = false;
+  #   };
+  # };
 
-  config = mkIf config.snowfall.sops.inheritKeyFile {
-    # Set up sops-nix and decrypt the age key.
-    sops = {
-      age = config.sops.secrets."keys/age".path;
-      defaultSopsFile = ../../../secrets/secrets.yaml;
-        defaultSopsFormat = "yaml";
-        # secrets."keys/age".path = mkIf config.snowfall.sops.inheritKeyFile keyFile;
-      };
-    };
+  # config = mkIf config.snowfall.sops.inheritKeyFile {
+  #   # Set up sops-nix and decrypt the age key.
+  #   sops = {
+  #     # age = config.sops.secrets."keys/age".path;
+  #     defaultSopsFile = ../../../secrets/secrets.yaml;
+  #       defaultSopsFormat = "yaml";
+  #       # secrets."keys/age".path = mkIf config.snowfall.sops.inheritKeyFile keyFile;
+  #     };
+  #   };
 }
