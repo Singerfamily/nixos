baee3c5983635af02910b9801bb125b0a43007c7
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-09-05 12:31:12 -0600

    refactor: update sops configuration to enable Age key decryption

--- modules/home/sops/default.nix
+++ modules/home/sops/default.nix
@@ -14,13 +14,13 @@ with lib;
   # HACK: For some reason, importing it in flake.nix fails, but works here...
   # imports = with inputs; [ sops-nix.homeManagerModules.sops ];
 
-  # options.snowfall.sops = {
-  #   # Whether to decrypt and use the Age key from lib/secrets.yaml.
-  #   inheritKeyFile = mkOption {
-  #     type = with types; bool;
-  #     default = true;
-  #   };
-  # };
+  options.snowfall.sops = {
+    # Whether to decrypt and use the Age key from lib/secrets.yaml.
+    inheritKeyFile = mkOption {
+      type = with types; bool;
+      default = false;
+    };
+  };
 
   config = mkIf config.snowfall.sops.inheritKeyFile {
     # Set up sops-nix and decrypt the age key.
