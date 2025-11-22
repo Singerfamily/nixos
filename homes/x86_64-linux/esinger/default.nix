36897449ce5e8ced0d09bd99a011ba6b01920eb1
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2025-11-22 11:03:38 -0700

    refactor: enhance user configuration with mkDefault for improved defaults and readability

--- homes/x86_64-linux/esinger/default.nix
+++ homes/x86_64-linux/esinger/default.nix
@@ -4,6 +4,8 @@
   config,
   ...
 }:
+
+with lib;
 let
   dotfiles = builtins.path {
     path = ./dotfiles;
@@ -15,54 +17,56 @@ in
 {
   snowfall = {
     user = {
-      fullName = "Eric Singer";
-      # email = "eric@singerfamily.ca";
+      fullName = mkDefault "Eric Singer";
+      # email = mkDefault  "eric@singerfamily.ca";
     };
   };
 
   home.stateVersion = "25.05";
 
-  home.shellAliases = lib.mkForce {
-    ls = "${pkgs.eza}/bin/eza --group-directories-first --color=auto --hyperlink";
-    ll = "ls -la";
-    lla = "ls -la --all";
-    tree = "ls --tree --level=3";
-    grep = "rg";
-    cat = "bat";
-    df = "duf";
-    du = "dua";
-    pgrep = "pgrep -a"; # Show full command lines.
+  home.shellAliases = {
+    ls = mkDefault "${pkgs.eza}/bin/eza --group-directories-first --color=auto --hyperlink";
+    ll = mkDefault "ls -la";
+    lla = mkDefault "ls -la --all";
+    tree = mkDefault "ls --tree --level=3";
+    grep = mkDefault "rg";
+    cat = mkDefault "bat";
+    df = mkDefault "duf";
+    du = mkDefault "dua";
+    pgrep = mkDefault "pgrep -a"; # Show full command lines.
+    dc = mkDefault "docker compose";
+    run = mkDefault "NIXPKGS_ALLOW_UNFREE=1 nix run --impure nixpkgs#$1";
   };
 
   programs = {
-    zsh.plugins = [
+    zsh.plugins = mkDefault [
       {
-        name = "powerlevel10k-config";
-        src = lib.cleanSource dotfiles;
-        file = "p10k.zsh";
+        name = mkDefault "powerlevel10k-config";
+        src = mkDefault lib.cleanSource dotfiles;
+        file = mkDefault "p10k.zsh";
       }
     ];
 
     git = {
-      enable = true;
-      userName = "LeaderbotX400";
-      userEmail = "34589843+LeaderbotX400@users.noreply.github.com";
-      attributes = [
+      enable = mkDefault true;
+      userName = mkDefault "LeaderbotX400";
+      userEmail = mkDefault "34589843+LeaderbotX400@users.noreply.github.com";
+      attributes = mkDefault [
         "secrets/*.{yaml,json,ini,env} diff=sopsdiffer"
       ];
 
       extraConfig = {
-        diff.sopsdiffer.textconv = "sops -d --config /dev/null";
+        diff.sopsdiffer.textconv = mkDefault "sops -d --config /dev/null";
       };
     };
 
     eza = {
-      enable = true;
-      git = true;
-      enableZshIntegration = true;
+      enable = mkDefault true;
+      git = mkDefault true;
+      enableZshIntegration = mkDefault true;
     };
 
-    lazydocker.enable = true;
+    lazydocker.enable = mkDefault true;
 
     nh = {
       enable = true;
@@ -74,18 +78,18 @@ in
     };
   };
 
-  sops = {
-    secrets."ssh/privateKey" = {
+  sops.secrets = {
+    "ssh/privateKey" = {
       path = "/home/${username}/.ssh/id_ed25519";
       mode = "0600";
     };
 
-    secrets."ssh/publicKey" = {
+    "ssh/publicKey" = {
       path = "/home/${username}/.ssh/id_ed25519.pub";
       mode = "0600";
     };
 
-    secrets."keys/age" = {
+    "keys/age" = {
       path = "/home/${username}/.config/sops/age/keys.txt";
       mode = "0600";
     };
