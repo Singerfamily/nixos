ec9c19fc2cc99925d1958e0ddb5c1d4ab1627cb8
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2026-03-31 18:03:28 -0600

    reset

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     import-tree.url = "github:vic/import-tree";
 
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
-  };
+  }
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
 }
