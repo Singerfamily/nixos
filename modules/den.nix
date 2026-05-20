8b1cb5fd7564d7f63ce4677d7f1c2db7c9e3b1fe
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2026-05-20 16:18:05 -0600

    fix broken refs

--- modules/den.nix
+++ modules/den.nix
@@ -5,13 +5,11 @@
   ...
 }:
 {
-  file-file.inputs.den.url = lib.mkDefault "github:denful/den";
+  flake-file.inputs.den.url = lib.mkDefault "github:denful/den";
+
   imports = [ inputs.den.flakeModule ];
 
   den.schema.user.classes = lib.mkDefault [ "homeManager" ];
-
-  # Enable angle bracket syntax (https://den.denful.dev/guides/angle-brackets)
-  config._module.args.__findFile = den.lib.__findFile;
 
   # https://den.denful.dev/reference/batteries
   den.default.includes = with den.batteries; [
