3d022564a553c9f658464431a448f3b81f9073aa
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2026-05-20 17:20:33 -0600

    add docker module configuration

---
+++ modules/aspects/services/docker.nix
@@ -0,0 +1,27 @@
+{ ... }:
+{
+  den.aspects.containers = impl: {
+    nixos =
+      { ... }:
+      {
+        virtualisation.docker = {
+          enable = true;
+          autoPrune.enable = true;
+          daemon.settings = {
+            # Enable CDI (Container Device Interface) for GPU support
+            features.cdi = true;
+            live-restore = true;
+          };
+          rootless = {
+            enable = true;
+            setSocketVariable = true;
+          };
+        };
+      };
+    homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = [ pkgs.lazydocker ];
+      };
+  };
+}
