635282ee85c5be180a2fd8c8d5a125d183c62735
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2026-05-20 16:48:07 -0600

    add disko overlay

--- modules/inputs/disko.nix
+++ modules/inputs/disko.nix
@@ -9,4 +9,12 @@
   imports = [
     inputs.disko.nixosModules.disko
   ];
+
+  # Overlay that provides disko and disko-doc packages from the disko flake input
+  flake.overlays.disko = _final: prev: {
+    inherit (inputs.disko.packages.${prev.system})
+      disko
+      disko-doc
+      ;
+  };
 }
