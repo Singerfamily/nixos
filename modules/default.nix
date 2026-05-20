96066cc9af4c918e2294d0e885eaf5e4af384ab5
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2026-05-20 17:03:51 -0600

    add nixos-wsl input to flake-file.inputs

--- modules/default.nix
+++ modules/default.nix
@@ -3,5 +3,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager.url = "github:nix-community/home-manager";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 }
