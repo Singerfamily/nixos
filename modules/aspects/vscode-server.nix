13aad4b73a2e26dc299a5349b603e64c0368b084
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2026-05-20 17:03:55 -0600

    add vscode-server module configuration

---
+++ modules/aspects/vscode-server.nix
@@ -0,0 +1,17 @@
+{ inputs, ... }:
+{
+  flake-file.inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
+  
+  den.aspects.vscode-server.nixos =
+    { pkgs, ... }:
+    {
+      imports = [ inputs.vscode-server.nixosModules.default ];
+      services.vscode-server.enable = true;
+
+      programs.nix-ld.enable = true;
+
+      environment.systemPackages = with pkgs; [
+        wget
+      ];
+    };
+}
