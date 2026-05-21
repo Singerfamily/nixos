2331a6b654f62f11a8b28c48f44d6d5796a36fe2
LeaderbotX400 <34589843+LeaderbotX400@users.noreply.github.com>
2026-05-21 09:39:45 -0600

    enable limine

--- modules/hosts/event-horizon/hardware.nix
+++ modules/hosts/event-horizon/hardware.nix
@@ -1,19 +1,18 @@
-_:
-{
-  den.aspects.event-horizon.nixos =
-    _:
-    {
-      boot.loader.systemd-boot.enable = true;
-      boot.loader.efi.canTouchEfiVariables = true;
+_: {
+  den.aspects.event-horizon.nixos = _: {
+    # boot.loader.systemd-boot.enable = true;
+    # boot.loader.efi.canTouchEfiVariables = true;
 
-      boot.initrd.availableKernelModules = [
-        "nvme"
-        "xhci_pci"
-        "ahci"
-        "usbhid"
-        "sd_mod"
-      ];
-      boot.kernelModules = [ "kvm-amd" ];
-      networking.hostId = "edc49e33";
-    };
+    boot.loader.limine.enable = true;
+
+    boot.initrd.availableKernelModules = [
+      "nvme"
+      "xhci_pci"
+      "ahci"
+      "usbhid"
+      "sd_mod"
+    ];
+    boot.kernelModules = [ "kvm-amd" ];
+    networking.hostId = "edc49e33";
+  };
 }
